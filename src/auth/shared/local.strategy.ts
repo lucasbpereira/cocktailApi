a42f912a6d76de19de93c7cfc66e20a29cd274b4
Lucas Barbosa <lucasbpereira@outlook.com>
2021-07-30 15:31:36 -0300

    Authenticate bug fixed

--- src/auth/shared/local.strategy.ts
+++ src/auth/shared/local.strategy.ts
@@ -7,14 +7,13 @@ import { AuthService } from './auth.service';
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
     super({
-      userNameField: 'email',
+      usernameField: 'email',
       passwordField: 'password',
     });
   }
 
   async validate(email: string, password: string): Promise<any> {
     const user = await this.authService.validateUser(email, password);
-
     if (!user) {
       throw new UnauthorizedException();
     }
